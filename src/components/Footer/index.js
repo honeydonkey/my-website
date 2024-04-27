a7bdfabc9361ebfea6b4b5da1a60212940e49224
honeydonkey <meiwwwwwww@gmail.com>
2024-04-28 00:37:43 +0800

    test

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Sanket Deshmukh</Logo>
+        <Logo>Marianne Wong</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
@@ -97,13 +97,10 @@ function Footer() {
           <NavLink href="#education">Education</NavLink>
         </Nav>
         <SocialMediaIcons>
-          <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon>
-          <SocialMediaIcon href={Bio.twitter} target="display"><TwitterIcon /></SocialMediaIcon>
           <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon>
-          <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2024 Sanket Deshmukh. All rights reserved.
+          &copy; 2024 Marianne Wong. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
